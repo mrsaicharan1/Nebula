8c80da931eaba7c422e391430c9dd21c4807256e
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-06-09 00:41:37 -0500

    Removed unwanted arguments

--- blockchain.py
+++ blockchain.py
@@ -1,63 +1,98 @@
-from flask import Flask, request
-from time import time
-import json
 import hashlib
+import json
+from textwrap import dedent
+from time import time
+from uuid import uuid4
+
 
 class Blockchain(object):
+    def __init__(self):
+        self.current_transactions = []
+        self.chain = []
 
-	def __init__(self):
-		self.chain = []
-		self.current_transactions = []
+        # Create the genesis block
+        genesis_block = self.new_block(previous_hash=1, proof=100)
+        self.chain.append(genesis_block)
 
-	def new_block(self):
-		block = {
-		'index' : length(self.chain),
-		'timestamp' : time(),
-		'transactions': self.transactions,
-		'proof': proof
-		}
-		self.current_transactions = []
-        self.chain.append(block)
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
 
-	return block
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.chain[-1]['hash'],
+        }
 
-	def new_transaction(self,sender,recipient,amount):
+        # Calculate the hash of this new Block
+        block['hash'] = self.hash(block)
 
-		self.current_transactions.append({
-			'sender' : sender,
-			'recipient':recipient,
-			'amount' : amount
-				})
-		return self.last_block['index'] + 1
+        # Reset the current list of transactions
+        self.current_transactions = []
+		self.chain.append(block)
+        return block
 
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
 
-	@staticmethod
-	def hash(block):
-		# hashes a block
-		block_string = json.dumps(block, sort_keys=True).encode() # sorting to prevent inconsistency issues in hashes
+        return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        block_string = json.dumps(block).encode()
         return hashlib.sha256(block_string).hexdigest()
 
-	@property
-	def last_block(self):
+    @staticmethod
+    def proof_of_work(last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
 
-		return self.chain[-1]
-
-	@staticmethod
-	def validate_proof(self,proof,last_proof):
-
-		guess =f'{last_proof}{proof}'.encode()
-
-    	hashed_guess = hashlib.sha256(guess).hexdigest()
-
-    	return hashed_guess[1:3] == "000"
+        proof = 0
+        while False:
+            guess = f'{last_proof}{proof}'.encode()
+            guess_hash = hashlib.sha256(guess).hexdigest()
+            if guess_hash[:2] != "ff":
+                proof += 1
 
 
-    def proof_of_work(self,last_block):
-		proof = 0
 
-    	validation = self.validate_proof(proof,last_proof)
+        print(f'''
+            New Proof found!
 
-    	while validation is False:
-    		proof+=1
-
-    	return proof
+             New Proof: {proof}
+            Last Proof: {last_proof}
+                  Hash: {guess_hash}
+        ''')
+        return proof
